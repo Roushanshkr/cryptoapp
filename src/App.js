914c005bca433f8c9ce666e921f9ddbcc7c61287
Roushanshkr <roushanshkr@gmail.com>
2025-03-15 23:16:45 +0530

    Add basename to BrowserRouter for GitHub Pages routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
 import React from 'react';
-import { Switch, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
-import './App.css'
+import './App.css';
+
 const App = () => {
   return (
     <div className='app'>
       <div className='navbar'>
-            <Navbar />
+        <Navbar />
       </div>
       <div className='main'>
-            <Layout>
-                  <div className='routes'>
-                  <Routes>
-  <Route path="/" element={<Homepage />} />  {/* ✅ Wrapped inside <Route> */}
-  <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
-  <Route path="/crypto/:coinId" element={<CryptoDetails />} />
-  <Route path="/exchanges" element={<Exchanges />} />
-  <Route path="/news" element={<News />} />
-</Routes>
-
-
-                  </div>
-            </Layout>
-      
-      <div className='footer'>
-            <Typography.Title  level={5} style={{ color: 'white', textAlign: 'center' }}>
-                  Cryptoverse <br />
-                  All rights reserved
-            </Typography.Title>
-            <Space>
-                  <Link to="/">Home</Link>
-                  <Link to="/exchanges">Exchanges</Link>
-                  <Link to="/news">News</Link>
-            </Space>
-            </div>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
-
   );
-}
+};
 
-export default App
+export default () => (
+  <BrowserRouter basename="/cryptoapp">
+    <App />
+  </BrowserRouter>
+);
